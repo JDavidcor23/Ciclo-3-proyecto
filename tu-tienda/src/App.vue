29ffe5ff560c94e06681d2ef3e945ffbc8d420d1
Jorge David Diaz Cordero <72902196+JDavidcor23@users.noreply.github.com>
2021-09-26 15:08:54 -0500

    pagina responsive

---
+++ tu-tienda/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <navbar/>
+    <carousel/>
+    <productos/>
+    <news/>
+    <tiendas/>
+    <pie/>
+  </div>
+</template>
+
+<script>
+import Carousel from './components/Carousel.vue'
+import Navbar from './components/Navbar.vue'
+import News from './components/News.vue'
+import Productos from './components/Productos.vue'
+import Tiendas from './components/Tiendas.vue'
+import Pie from './components/Pie.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Carousel,
+    Navbar,
+    Productos,
+    News,
+    Tiendas,
+    Pie
+
+    },
+    News
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ tu-tienda/src/components/News.vue
@@ -0,0 +1,203 @@
+<template>
+     <section id="news">
+        <main class="contenido">
+            <h2>Nuestro Blog Saludable</h2>
+            <p>
+                Muchos creemos que llevar una vida saludable, una dieta sana y una rutina de ejercicios es cada vez más
+                difícil con lo demandante que se ha vuelto nuestra sociedad. Las largas jornadas laborales, el estrés
+                del tráfico, las actividades diarias, la familia y muchos otros aspectos más son factores que pueden
+                limitar la práctica de buenos hábitos de salud si no se tiene la información correcta.
+            </p>
+            <p>
+                Sin embargo, y gracias a Internet tenemos el poder de la información a nuestro alcance y queda a nuestro
+                criterio sacarle el mayor provecho posible. Por ello, si te has planteado el objetivo de mejorar tu
+                salud y conseguir un estilo de vida más saludable, pero no sabes por dónde empezar, te recomendamos
+                revisar nuestra lista con los mejores consejos sobre este importante tema.
+            </p>
+            <p>
+                En virtud de la gran cantidad de sitios en los que puedes obtener información, muchas veces errada o sin
+                fundamentos médicos, hemos hecho una investigación previa sobre los blogs elegidos y sus autores para
+                verificar que sus contenidos son acertados y que la información que ofrecen te ayudará a mejorar tu
+                salud a partir de una dieta equilibrada, mejores hábitos y una rutina de ejercicios acorde a tus
+                necesidades.
+            </p>
+            <p>
+                En este blog encontrarás información diversa sobre un gran número de temas, además de recomendaciones
+                sobre diferentes aspectos relacionados con la salud. Si quieres comenzar a alimentarte de una manera más
+                saludable, podrás encontrar recetas cocinadas con la plancha de asar o con la vaporera de silicona, y
+                también averiguarás más sobre el complejo vitamínico y sus beneficios, así como el zumo de aloe vera,
+                por ejemplo. Si deseas empezar a hacer deporte, debes saber que del mismo modo existe una gran cantidad
+                de artículos sobre correr, trotar, nadar, así como datos sobre artículos que podrías adquirir, como un
+                reloj running o un podómetro.
+            </p>
+            <p>
+                Tener este blog como referencia te permitirá ser constante en tu determinación por alcanzar una mejor
+                calidad de vida al conocer los tipos de alimentos que debes consumir, qué ejercicios puedes realizar a
+                diario sin alterar el ritmo de tus actividades diarias e ir cambiando poco a poco los hábitos de tu
+                familia
+            </p>
+        </main>
+        <aside class="sidebar">
+            <h2>Blog Recomendado</h2>
+            <h2>Adelgaza con Susi</h2>
+            <p>Susi es dietista y por su experiencia con el manejo del sobrepeso decidió desarrollar este blog para
+                aportar a su comunidad de lectores recetas y menús nutritivos para perder peso. La autora admite que
+                no
+                hay fórmulas mágicas ni alimentos exclusivos para adelgazar, por ello recomienda cambiar los hábitos
+                alimenticios por unos más saludables, aprender a comer mejor y mantener la nueva rutina para
+                alcanzar el
+                peso deseado. Muchas de las recetas son de su propia inspiración al igual que las fotos de los
+                platos
+                terminados, además comparte tips, consejos y hasta videos de ejercicios entretenidos que puedes
+                realizar
+                en casa para complementar las dietas.
+                <a href="https://cutt.ly/OEzQBx8">Adelgaza con sushi</a>
+            </p>
+        </aside>
+        <div class="widget-1">
+
+            <h2>Diferencias entre fruta y verdura</h2>
+            <p> Antes de explicar cuales son las diferencias entre frutas y verduras, lo primero que hay quedecir es que
+                no son términos científicos. Fruta y verdura podríamos decir que es una terminología coloquial.</p>
+            <p>
+                Y no se trata de ninguna de estas viejas ideas que a menudo pensamos:
+
+                * las frutas son dulces y las verduras no,
+
+                * las frutas son más jugosas que las verduras,
+
+                * las frutas son las que se comen en el postre,
+            </p>
+            <p>las verduras son de color verde.</p>
+            <p>Esta es la verdadera diferencia entre fruta y verdura:</p>
+            <p>
+                Una fruta es la parte del fruto que nos comemos, es decir, el ovario fecundado que la planta
+                crea
+                alrededor de la semilla o semillas. La fruta es siempre parte del fruto y, por tanto, tiene
+                siempre
+                una función reproductiva en la planta.
+                Una verdura es cualquier otra parte de la planta que no sea el fruto y que consumimos como
+                alimento.
+                Puede tratarse de las hojas, la raíz, los tallos o incluso las flores, en todos estos casos
+                se
+                considera verdura.</p>
+        </div>
+        <div class="widget-2">
+            <h2>Beneficios de las Frutas</h2>
+            Las frutas y verduras son esenciales para cubrir los requerimientos nutricionales de nuestro
+            organismo
+            debido a su alto contenido en:
+            <br>
+            * Fibra
+            <br>
+            * Vitaminas
+            <br>
+            * Minerales
+            <br>
+            * Agua
+            <br>
+            * y Antioxidantes
+            <br>
+            * y además no contienen colesterol y aportan pocas calorías.
+        </div>
+    </section>
+</template>
+<script>
+
+</script>
+<style>
+    :root {
+  --hueso: #faf7ee;
+  --verde: #2f7a2c;
+  --amarillo: #ffab49;
+  --gris-oscuro: #444444;
+}
+
+#news {
+  width: 100%;
+  height: auto;
+  /* margin: 0 auto; */
+  display: flex;
+  text-align: left;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+#news .contenido {
+  color: #faf7ee;
+  font-family: "poppins", sans-serif;
+  width: 310px;
+  max-width: 500px;
+  border-radius: 10px;
+  background: var(--verde);
+  padding: 30px;
+  margin-top: 30px;
+  box-shadow: 1px 6px 21px -1px #000000;
+}
+#news .contenido p {
+  padding: 5px;
+}
+.sidebar {
+  width: 300px;
+  max-width: 500px;
+  font-family: "poppins", sans-serif;
+  color: #faf7ee;
+  border-radius: 10px;
+  padding: 30px;
+  height: auto;
+  margin-top: 30px;
+  background: var(--verde);
+  box-shadow: 1px 6px 21px -1px #000000;
+}
+ h2 {
+  font-size: 1.4rem;
+  background: var(--amarillo);
+  padding: 10px;
+  border-radius: 10px;
+}
+
+.widget-1,
+.widget-2 {
+  width: 300px;
+  max-width: 500px;
+  color: #faf7ee;
+  font-family: "poppins", sans-serif;
+  border-radius: 10px;
+  height: auto;
+  background: var(--verde);
+  padding: 30px;
+  box-shadow: 1px 6px 21px -1px #000000;
+}
+.widget-1 {
+  /* grid-column: 1/3; */
+  margin-top: 30px;
+}
+.widget-2 {
+  margin-top: 30px;
+}
+.widget-2 p {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin: 0;
+}
+@media (min-width: 600px){
+#news {
+  display: flex;
+  text-align: left;
+  flex-direction: column;
+  align-items: center;
+}
+}
+@media (min-width: 700px){
+#news .contenido {
+  width: 500px;
+
+}
+.sidebar{
+  width: 500px;
+}
+.widget-1,
+.widget-2{
+  width: 500px;
+}
+}
+</style>
